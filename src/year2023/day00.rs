33434289925db07ae06bcc4ddbf6c94a47e94acf
Ben Smith <bh2smith@gmail.com>
2023-12-03 19:27:57 +0100

    day 1 solution

---
+++ src/year2023/day00.rs
@@ -0,0 +1,23 @@
+
+pub fn puzzle1(_input: &str) -> u32 {
+    0
+}
+
+pub fn puzzle2(_input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn puzzle1() {
+        assert_eq!(super::puzzle1(""), 0);
+    }
+
+    #[test]
+    fn puzzle2() {
+        assert_eq!(super::puzzle2(""), 0);
+    }
+}
