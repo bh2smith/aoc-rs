75602a202a5066b6bf0022eb54b585702f317a92
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-10-22 12:13:43 +0200

    rustfmt day03

--- src/year2016/day03.rs
+++ src/year2016/day03.rs
@@ -1,39 +1,38 @@
 use itertools::Itertools;
 use std::convert::TryInto;
 
-
 #[derive(Debug)]
 struct Trip {
-  x: u32, 
-  y: u32,
-  z: u32
+    x: u32,
+    y: u32,
+    z: u32,
 }
 
 impl Trip {
-  fn triangle_inequality(&self) -> bool {
-    self.x + self.y > self.z
-  }
+    fn triangle_inequality(&self) -> bool {
+        self.x + self.y > self.z
+    }
 }
 
 impl From<&str> for Trip {
     fn from(s: &str) -> Self {
-      let nums = s
-                .split_whitespace()
-                .map(|t| t.parse::<u32>().unwrap())
-                .collect_vec();
+        let nums = s
+            .split_whitespace()
+            .map(|t| t.parse::<u32>().unwrap())
+            .collect_vec();
         // Convert Vec<u32> -> [u32; 3] -> (u32, u32, u32)
-        let mut arr: [u32; 3] = nums.try_into().expect("Each line must have exactly 3 numbers");
+        let mut arr: [u32; 3] = nums
+            .try_into()
+            .expect("Each line must have exactly 3 numbers");
         // Largest will always be z.
         arr.sort();
         Self {
-          x: arr[0],
-          y: arr[1],
-          z: arr[2]
+            x: arr[0],
+            y: arr[1],
+            z: arr[2],
         }
     }
 }
-
-
 
 pub fn puzzle1(input: &str) -> usize {
     // parse rows:
@@ -42,7 +41,6 @@ pub fn puzzle1(input: &str) -> usize {
 }
 
 pub fn puzzle2(input: &str) -> usize {
-
     // parsing colum defined trips
     let rows: Vec<Vec<&str>> = input
         .lines()
@@ -56,9 +54,9 @@ pub fn puzzle2(input: &str) -> usize {
         if chunk.len() == 3 {
             // Transpose: zip each column downward
             for i in 0..chunk[0].len() {
-            let line = format!("{} {} {}", chunk[0][i], chunk[1][i], chunk[2][i]);
-            trips.push(Trip::from(line.as_str()));
-        }
+                let line = format!("{} {} {}", chunk[0][i], chunk[1][i], chunk[2][i]);
+                trips.push(Trip::from(line.as_str()));
+            }
         }
     }
     trips.iter().filter(|t| t.triangle_inequality()).count()
